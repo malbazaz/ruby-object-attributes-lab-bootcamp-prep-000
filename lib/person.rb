f8b39d2cd35437f304909b7ed3ec3a48a8828b02
Mo Al <malbazaz@mba2018.hbs.edu>
2018-06-10 04:50:26 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name= (persons_name)
+  @personsname = persons_name
+  end
+  def name
+  @personsname
+  end
+  def job= (person_job)
+  @personsjob = person_job
+  end
+  def job
+  @personsjob 
+  end 
+end 
